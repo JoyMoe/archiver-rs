bb433cbf34289aff0edd8d98463238db7d608c39
Cédric Meuter <cedric.meuter@gmail.com>
2023-09-12 17:47:54 +0200

    test: added integration tests for Gzip, Bzip2 and Xz

---
+++ tests/compressed.rs
@@ -0,0 +1,38 @@
+use std::path::Path;
+
+use archiver_rs::{Bzip2, Compressed, Gzip, Xz};
+
+type Error = Box<dyn std::error::Error>;
+type Result<T> = std::result::Result<T, Error>;
+
+fn test_decompress(mut compressed: impl Compressed, expected: &str) -> Result<()> {
+    let sandbox = tempfile::tempdir()?;
+    let decompressed_file_path = sandbox.path().join("sample.txt");
+    compressed.decompress(Path::new(&decompressed_file_path))?;
+    assert_eq!(std::fs::read_to_string(decompressed_file_path)?, expected);
+    Ok(())
+}
+
+#[test]
+fn test_gzipped_decompress() -> Result<()> {
+    test_decompress(
+        Gzip::open(Path::new("tests/sample/sample.txt.gz"))?,
+        "sample\n",
+    )
+}
+
+#[test]
+fn test_bzipped_decompress() -> Result<()> {
+    test_decompress(
+        Bzip2::open(Path::new("tests/sample/sample.txt.bz2"))?,
+        "sample\n",
+    )
+}
+
+#[test]
+fn test_xzipped_decompress() -> Result<()> {
+    test_decompress(
+        Xz::open(Path::new("tests/sample/sample.txt.xz"))?,
+        "sample\n",
+    )
+}
